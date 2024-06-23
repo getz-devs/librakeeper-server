2b9ec89315bffac01ac25f903f620b051ec486e9
Sergeydigl3 <26508358+Sergeydigl3@users.noreply.github.com>
2024-06-23 20:56:51 +0300

    added base structure

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # librakeeper-server
+
+```shell
+protoc --go_out=. --go_opt=paths=source_relative --go-grpc_out=. --go-grpc_opt=paths=source_relative proto/search.proto
+```
+
+```shell
+protoc --go-grpc_out=. librakeeper-server/pkg/pb/book.proto
+```
---
+++ internal/searcher/config/config.go
@@ -0,0 +1,60 @@
+package config
+
+import (
+	"flag"
+	"github.com/ilyakaznacheev/cleanenv"
+	"os"
+	"time"
+)
+
+type Config struct {
+	Env string `yaml:"env" env-default:"local"`
+
+	StoragePath string `yaml:"storage_path" env-required:"true"`
+
+	GRPC GRPCConfig `yaml:"grpc"`
+}
+
+type GRPCConfig struct {
+	Port    int           `yaml:"port" env-default:"44044"`
+	Timeout time.Duration `yaml:"timeout" env-default:"10h"`
+}
+
+func MustLoad() *Config {
+	path := fetchConfigPath()
+	if path == "" {
+		panic("config path is empty")
+	}
+
+	if _, err := os.Stat(path); os.IsNotExist(err) {
+		panic("config file doesn't exist: " + path)
+	}
+
+	var cfg Config
+
+	err := cleanenv.ReadConfig(path, &cfg)
+	if err != nil {
+		panic("failed to read config: " + err.Error())
+	}
+
+	return &cfg
+}
+
+// fetchConfigPath fetches config path from command line flag or environment variable
+// Priority: command line flag > environment variable > default
+func fetchConfigPath() string {
+	var res string
+
+	flag.StringVar(&res, "config", "", "path to config file")
+	flag.Parse()
+
+	if res == "" {
+		res = os.Getenv("CONFIG_PATH")
+	}
+
+	//if res == "" {
+	//	res = "config.yml"
+	//}
+
+	return res
+}
