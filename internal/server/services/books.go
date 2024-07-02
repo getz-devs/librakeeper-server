13f11ca45bec224715ed811494164a1c6c04ee72
Den <denis100703@gmail.com>
2024-07-02 18:32:59 +0300

    created services for CRUD ops

---
+++ internal/server/services/books.go
@@ -0,0 +1,133 @@
+package services
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"github.com/getz-devs/librakeeper-server/internal/server/models"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"log/slog"
+	"time"
+)
+
+var (
+	ErrBookNotFound = errors.New("book not found")
+)
+
+type BookService struct {
+	collection *mongo.Collection
+	log        *slog.Logger
+}
+
+func NewBookService(collection *mongo.Collection, log *slog.Logger) *BookService {
+	return &BookService{
+		collection: collection,
+		log:        log,
+	}
+}
+
+func (s *BookService) CreateBook(ctx context.Context, book *models.Book) (*models.Book, error) {
+	book.CreatedAt = time.Now()
+	book.UpdatedAt = time.Now()
+
+	res, err := s.collection.InsertOne(ctx, book)
+	if err != nil {
+		s.log.Error("failed to create book", slog.Any("error", err))
+		return nil, fmt.Errorf("failed to create book: %w", err)
+	}
+
+	book.ID = res.InsertedID.(primitive.ObjectID)
+	return book, nil
+}
+
+func (s *BookService) GetBook(ctx context.Context, bookID primitive.ObjectID) (*models.Book, error) {
+	var book models.Book
+	err := s.collection.FindOne(ctx, bson.M{"_id": bookID}).Decode(&book)
+	if err != nil {
+		if errors.Is(err, mongo.ErrNoDocuments) {
+			return nil, ErrBookNotFound
+		}
+		return nil, fmt.Errorf("failed to get book: %w", err)
+	}
+	return &book, nil
+}
+
+func (s *BookService) GetBooks(ctx context.Context, page int64, limit int64) ([]*models.Book, error) {
+	findOptions := options.Find()
+	findOptions.SetSkip((page - 1) * limit)
+	findOptions.SetLimit(limit)
+
+	cursor, err := s.collection.Find(ctx, bson.M{}, findOptions)
+	if err != nil {
+		s.log.Error("failed to get books", slog.Any("error", err))
+		return nil, fmt.Errorf("failed to get books: %w", err)
+	}
+	defer cursor.Close(ctx)
+
+	var books []*models.Book
+	for cursor.Next(ctx) {
+		var book models.Book
+		if err := cursor.Decode(&book); err != nil {
+			return nil, fmt.Errorf("failed to decode book: %w", err)
+		}
+		books = append(books, &book)
+	}
+
+	if err := cursor.Err(); err != nil {
+		return nil, fmt.Errorf("cursor error: %w", err)
+	}
+	return books, nil
+}
+
+func (s *BookService) GetBooksByBookshelfID(ctx context.Context, bookshelfID primitive.ObjectID, page int64, limit int64) ([]*models.Book, error) {
+	matchStage := bson.D{{"$match", bson.D{{"bookshelf_id", bookshelfID}}}}
+	skipStage := bson.D{{"$skip", (page - 1) * limit}}
+	limitStage := bson.D{{"$limit", limit}}
+
+	cursor, err := s.collection.Aggregate(ctx, mongo.Pipeline{matchStage, skipStage, limitStage})
+	if err != nil {
+		s.log.Error("failed to get books by bookshelf id", slog.Any("error", err))
+		return nil, fmt.Errorf("failed to get books by bookshelf id: %w", err)
+	}
+	defer cursor.Close(ctx)
+
+	var books []*models.Book
+	for cursor.Next(ctx) {
+		var book models.Book
+		if err := cursor.Decode(&book); err != nil {
+			return nil, fmt.Errorf("failed to decode book: %w", err)
+		}
+		books = append(books, &book)
+	}
+
+	if err := cursor.Err(); err != nil {
+		return nil, fmt.Errorf("cursor error: %w", err)
+	}
+	return books, nil
+}
+
+func (s *BookService) UpdateBook(ctx context.Context, bookID primitive.ObjectID, update bson.M) error {
+	update["updated_at"] = time.Now()
+	_, err := s.collection.UpdateOne(ctx, bson.M{"_id": bookID}, bson.M{"$set": update})
+	if err != nil {
+		if errors.Is(err, mongo.ErrNoDocuments) {
+			return ErrBookNotFound
+		}
+		return fmt.Errorf("failed to update book: %w", err)
+	}
+	return nil
+}
+
+func (s *BookService) DeleteBook(ctx context.Context, bookID primitive.ObjectID) error {
+	_, err := s.collection.DeleteOne(ctx, bson.M{"_id": bookID})
+	if err != nil {
+		if errors.Is(err, mongo.ErrNoDocuments) {
+			return ErrBookNotFound
+		}
+		return fmt.Errorf("failed to delete book: %w", err)
+	}
+	return nil
+}
---
+++ internal/server/services/users.go
@@ -0,0 +1,83 @@
+package services
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"github.com/getz-devs/librakeeper-server/internal/server/models"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"log/slog"
+	"time"
+)
+
+var (
+	ErrUserAlreadyExists = errors.New("user already exists")
+	ErrUserNotFound      = errors.New("user not found")
+)
+
+type UserService struct {
+	collection *mongo.Collection
+	log        *slog.Logger
+}
+
+func NewUserService(collection *mongo.Collection, log *slog.Logger) *UserService {
+	return &UserService{
+		collection: collection,
+		log:        log,
+	}
+}
+
+func (s *UserService) CreateUser(ctx context.Context, user *models.User) (*models.User, error) {
+	user.CreatedAt = time.Now()
+	user.UpdatedAt = time.Now()
+
+	_, err := s.collection.InsertOne(ctx, user)
+	if err != nil {
+		// Check for duplicate key error (Firebase UID uniqueness)
+		var mongoErr mongo.WriteException
+		if errors.As(err, &mongoErr) && mongoErr.WriteErrors[0].Code == 11000 {
+			return nil, ErrUserAlreadyExists
+		}
+
+		s.log.Error("failed to create user", slog.Any("error", err))
+		return nil, fmt.Errorf("failed to create user: %w", err)
+	}
+
+	return user, nil
+}
+
+func (s *UserService) GetUserByID(ctx context.Context, userID string) (*models.User, error) {
+	var user models.User
+	err := s.collection.FindOne(ctx, bson.M{"_id": userID}).Decode(&user)
+	if err != nil {
+		if errors.Is(err, mongo.ErrNoDocuments) {
+			return nil, ErrUserNotFound
+		}
+		return nil, fmt.Errorf("failed to get user by id: %w", err)
+	}
+	return &user, nil
+}
+
+func (s *UserService) UpdateUser(ctx context.Context, userID string, update bson.M) error {
+	update["updated_at"] = time.Now()
+	_, err := s.collection.UpdateOne(ctx, bson.M{"_id": userID}, bson.M{"$set": update})
+	if err != nil {
+		if errors.Is(err, mongo.ErrNoDocuments) {
+			return ErrUserNotFound
+		}
+		return fmt.Errorf("failed to update user: %w", err)
+	}
+	return nil
+}
+
+func (s *UserService) DeleteUser(ctx context.Context, userID string) error {
+	_, err := s.collection.DeleteOne(ctx, bson.M{"_id": userID})
+	if err != nil {
+		if errors.Is(err, mongo.ErrNoDocuments) {
+			return ErrUserNotFound
+		}
+		return fmt.Errorf("failed to delete user: %w", err)
+	}
+	return nil
+}
